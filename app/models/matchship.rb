859547043534f964bbc7f292b8ba5e37727a7283
Glenn Martin <glennmartin76@glenns-MacBook-Air-2.local>
2013-06-02 14:38:49 +1000

    more changes

--- app/models/matchship.rb
+++ app/models/matchship.rb
@@ -5,11 +5,13 @@ class Matchship < ActiveRecord::Base
 
 
   belongs_to :match 
-  belongs_to :team 
+  
+  # belongs_to :team 
 
 
   has_many :teams
-  accepts_nested_attributes_for :teams
-  accepts_nested_attributes_for :team
+
+   accepts_nested_attributes_for :teams
+  # accepts_nested_attributes_for :team
 
 end
