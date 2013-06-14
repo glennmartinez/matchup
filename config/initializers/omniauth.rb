814aeb6ab695b156270b3f5a3f222a9ade7d3b48
Glenn Martin <glennmartin76@glenns-MacBook-Air-2.local>
2013-06-14 13:02:43 +1000

    changed key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, '45C1032773C0E6132C9B5D2C8C1D4FD9'
+  provider :steam, 'A6066443D64E0B5A1CF58ED46C4D6D66'
 
 # C08081F087208735DDA6C0D57679BF60
 end
 
-require "openid/fetchers"
-OpenID.fetcher.ca_file = "etc/ssl/certs/ca-certificates.crt"
+# require "openid/fetchers"
+# OpenID.fetcher.ca_file = "etc/ssl/certs/ca-certificates.crt"
