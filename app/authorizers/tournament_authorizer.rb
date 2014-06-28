58598942769662fa6a876605cd230f01d05a77e2
Glenn Martin <glennmartin76@gmail.com>
2014-06-28 15:46:48 +1000

    added some changes

--- app/authorizers/tournament_authorizer.rb
+++ app/authorizers/tournament_authorizer.rb
@@ -1,25 +1,25 @@
 class TournamentAuthorizer < ApplicationAuthorizer
   # Class method: can this user at least sometimes create a Schedule?
   def self.creatable_by?(user)
-    user.has_role? :admin
+    user.admin?
   end
 
   # Instance method: can this user delete this particular schedule?
   def self.deletable_by?(user)
     # resource.in_future? && user.admin? && resource.department == user.department
-    user.has_role? :admin
+    user.admin?
 
   end
 
   def self.readable_by?(user)
 
-  	user.has_role? :admin
+    user.admin?
   	
   end
 
   def self.updatable_by?(user)
 
-     user.has_role? :adminwewe
+    user.admin?
 
   end
 
